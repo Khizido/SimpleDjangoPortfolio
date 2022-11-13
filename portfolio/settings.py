c45945eae6fdffd482a1783a150a7d893c7ae668
khizido <khizido36@gmail.com>
2022-11-13 23:30:42 +0300

    s

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -112,8 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 
 # Default primary key field type
