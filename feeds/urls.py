ce58abac16df6a37a44b8df4486d65b41346b5ad
khizido <khizido36@gmail.com>
2022-11-13 22:45:35 +0300

    Update urls.py

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,21 +1,7 @@
-from django.conf import settings
-from django.conf.urls import patterns, url, include
-from django.contrib import admin
 from django.urls import path
-
 from . import views
 
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^$', include('myapp.cesar.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
 urlpatterns = [
     path('',views.home_page,name="main_home" )
 
 ]
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
