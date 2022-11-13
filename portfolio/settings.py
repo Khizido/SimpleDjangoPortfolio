9b8d49e7b62f7df407d6924aaf507b381e0a4e32
khizido <khizido36@gmail.com>
2022-11-13 23:37:47 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,9 +129,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-if DEBUG:
-        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+
 
 django_heroku.settings(locals())
