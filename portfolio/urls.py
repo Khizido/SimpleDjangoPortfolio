f0764aede02cd1178a7e5c6479aa09998173d143
khizido <khizido36@gmail.com>
2022-11-14 04:47:12 +0300

    n

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,14 @@
 
-from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('feeds.urls')),
     path('admin/', admin.site.urls),
+
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
